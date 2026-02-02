6424704f9c22e5b683ce7e90ef8567d9e4f846ee
WilsontheWolf <git@shorty.systems>
2026-02-02 12:30:57 -0700

    use gawk

--- collect.sh
+++ collect.sh
@@ -9,9 +9,9 @@ git pull > /dev/null
 export COMMITS=$(git log --pretty=format:"%h")
 
 for c in $COMMITS; do
-	export v=$(git show $c:version.lua 2>/dev/null | awk 'match($0, /return "(.+)"/,a) {print a[1]}')
+	export v=$(git show $c:version.lua 2>/dev/null | gawk 'match($0, /return "(.+)"/,a) {print a[1]}')
 	if [ -z "$v" ]; then
-		export v=$(git show $c:core/core.lua 2>/dev/null | awk 'match($0, /MODDED_VERSION = "(.+)"/,a) {print a[1]}')
+		export v=$(git show $c:core/core.lua 2>/dev/null | gawk 'match($0, /MODDED_VERSION = "(.+)"/,a) {print a[1]}')
 	fi
 	echo $c $v
 done
